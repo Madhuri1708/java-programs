ac21407bc2e1aac892525dd017060b03b909ac00
Gajji Madhuri <madhuri.madhu1708@gmail.com>
2025-05-01 11:05:51 +0530

    Add files via upload

---
+++ switchh.java
@@ -0,0 +1,44 @@
+class switchh{
+	public static void main(String[] args) {
+		int week=4;
+		String day;
+		switch(week){
+			case 1:
+				day="sunday";
+				break;
+			case 2:
+				day="monday";
+				break;
+			case 3:
+				day="tuesday";
+				break;
+			case 4:
+				day="wednesday";
+				break;
+			case 5:
+				day="thursday";
+				break;
+			case 6:
+				day="friday";
+				break;
+			case 7:
+				day="saturday";
+				break;
+			default:
+				day="Invalid day";
+				break;
+		}
+		System.out.println(day);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+

24248d8a517c8a737c1e1f25d2e56fc03a6bf8cd
Gajji Madhuri <madhuri.madhu1708@gmail.com>
2025-05-01 11:02:43 +0530

    Add files via upload

---
+++ Replacestr.java
@@ -0,0 +1,12 @@
+class Replacestr{
+	public static void main(String[] args){
+		String s1=" maths marks ";
+		String s2=" HELLO ";
+
+	System.out.println(s1.toUpperCase());
+	System.out.println(s2.toLowerCase());
+	System.out.println(s1.trim());
+
+
+	}
+}

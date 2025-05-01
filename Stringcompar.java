749b2046673230a767184957f01a16b06d966e66
Gajji Madhuri <madhuri.madhu1708@gmail.com>
2025-05-01 11:04:43 +0530

    Add files via upload

---
+++ Stringcompar.java
@@ -0,0 +1,16 @@
+class Stringcompar{
+	public static void main(String[] args){
+		String s1="Hell";
+		String s2="Hello";
+		String s3="Hello";
+		String s4="tonight";
+		System.out.println(s4.equalsIgnoreCase("TONIGHT"));
+		System.out.println("length of s4 is" +s4.length());
+
+		System.out.println(s1.equals(s2));
+		System.out.println(s2.equals(s3));
+		
+	}
+}
+
+
